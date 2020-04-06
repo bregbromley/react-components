3499f5091f61c7e0d0f44dc4e6ffa05ca4d2dcb5
Breg Bromley <bregbromley@gmail.com>
2020-04-06 13:09:14 -0600

    Completed basic requirements

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
+// // TODO
+// var Cucumbers = () => (
+//   <li>cucumbers</li>
+// );
+
+// var Kale = () => (
+//   <li>kale</li>
+// );
+
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.items[0]}</li>
+//     <li>{props.items[1]}</li>
+//   </ul>
+
+// );
+
+// var GroceryList = () => (
+//   <div>
+//     <GroceryListItem items={['cucumbers', 'kale']} />
+//   </div>
+// );
+
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
 // TODO
+// var Cucumbers = () => (
+//   <li>cucumbers</li>
+// );
+
+// var Kale = () => (
+//   <li>kale</li>
+// );
+
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li>{props.items[0]}</li>
+//     <li>{props.items[1]}</li>
+//   </ul>
+
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super (props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style ={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.items}</li>
+    );
+  }
+}
+
+
+
+var GroceryList = (props) => (
+  <div>
+    <ul>
+      {props.items.map(items =>
+        <GroceryListItem items={items} />
+        )}
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList items={['cucumber', 'kale', 'bread']} />, document.getElementById("app"));
